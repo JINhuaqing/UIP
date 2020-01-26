20cf3a2e143c1e125b795f9b1fc914a1a6bd754e
JinHuaqing <jinhuaqing@hku.hk>
2020-01-26 21:54:45 +0800

    update minor

--- NormSetting/UIPnormal_cpr_pymc.py
+++ NormSetting/UIPnormal_cpr_pymc.py
@@ -4,6 +4,8 @@ import pickle
 import argparse
 from utilities_norm import gen_post_full, gen_post_jef, getNPPcon, getLCPcon, getUIPJScon, getUIPDcon
 import pymc3 as pm
+import os.path as osp
+import time
 
 np.random.seed(2020)
 
@@ -21,7 +23,15 @@ thetas = [0.5, 1]
 ns = [50, 100]
 print(f"The theta0 is {theta0}.")
 
+fname = f"MCMCNorm_{Num}_p0{int(100*theta0)}_n{int(n)}.pkl" 
+if not osp.isfile(fname):
+    with open(fname, "wb") as f:
+        pickle.dump(results, f)
+with open(fname, "rb") as f:
+    results = pickle.load(f)
+numRes = len(results)
 
+init = time.time()
 for jj in range(Num):
     result = {}
     D = norm.rvs(loc=theta0, scale=sigma0, size=n)
@@ -34,72 +44,73 @@ for jj in range(Num):
                       "ns": ns
                       } 
 
-    # jeffrey's prior
-    post_normal_jef = gen_post_jef(55000, D=D)
-    result["jef"] = post_normal_jef
-
-    # full borrowing under jeffrey's prior
-    post_normal_full = gen_post_full(55000, D=D, Ds=Ds)
-    result["full"] = post_normal_full
-
     print("==" * 100)
-    # NPP prior
-    NPP_model = getNPPcon(D, Ds)
-    with NPP_model:
-        post_normal_NPP = pm.sample(draws=2500, tune=15000, target_accept=0.8, cores=4, chains=4)
-    result["NPP"] = post_normal_NPP
-    print("The results of NPP")
-    diverging = post_normal_NPP['diverging']
-    print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
-    diverging_pct = diverging.nonzero()[0].size / len(post_normal_NPP) * 100
-    print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
-    print(pm.summary(post_normal_NPP))
+    outstring = f"%The iteration {jj+1}/{Num}. Totol time is {np.round((time.time()-init)/60, 4)} min%"
+    print("%" * len(outstring))
+    print(outstring)
+    print("%" * len(outstring))
+    if jj >= numRes:
+        # jeffrey's prior
+        post_normal_jef = gen_post_jef(55000, D=D)
+        result["jef"] = post_normal_jef
 
-    print("--" * 100)
-    # LCP prior
-    LCP_model = getLCPcon(D, Ds)
-    with LCP_model:
-        post_normal_LCP = pm.sample(draws=2500, tune=15000, target_accept=0.9, cores=4, chains=4)
-    result["LCP"] = post_normal_LCP
-    print("The results of LCP")
-    diverging = post_normal_LCP['diverging']
-    print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
-    diverging_pct = diverging.nonzero()[0].size / len(post_normal_LCP) * 100
-    print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
-    print(pm.summary(post_normal_LCP))
+        # full borrowing under jeffrey's prior
+        post_normal_full = gen_post_full(55000, D=D, Ds=Ds)
+        result["full"] = post_normal_full
 
-    print("--" * 100)
-    # UIPD prior
-    UIPD_model = getUIPDcon(D, Ds)
-    with UIPD_model:
-        post_normal_UIPD = pm.sample(draws=2500,tune=15000, target_accept=0.8, cores=4, chains=4)
-    result["UIPD"] = post_normal_UIPD
-    print("The results of UIPD")
-    diverging = post_normal_UIPD['diverging']
-    print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
-    diverging_pct = diverging.nonzero()[0].size / len(post_normal_UIPD) * 100
-    print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
-    print(pm.summary(post_normal_UIPD))
+        # NPP prior
+        NPP_model = getNPPcon(D, Ds)
+        with NPP_model:
+            post_normal_NPP = pm.sample(draws=5000, tune=5000, target_accept=0.8, cores=4, chains=4)
+        result["NPP"] = post_normal_NPP
+        print("The results of NPP")
+        diverging = post_normal_NPP['diverging']
+        print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
+        diverging_pct = diverging.nonzero()[0].size / len(post_normal_NPP) * 100
+        print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
+        print(pm.summary(post_normal_NPP))
 
-    print("--" * 100)
-    # UIPJS prior
-    UIPJS_model = getUIPJScon(D, Ds)
-    with UIPJS_model:
-        post_normal_UIPJS = pm.sample(draws=2500, tune=15000, target_accept=0.8, cores=4, chains=4)
-    result["UIPJS"] = post_normal_UIPJS
-    print("The results of UIPJS")
-    diverging = post_normal_UIPJS['diverging']
-    print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
-    diverging_pct = diverging.nonzero()[0].size / len(post_normal_UIPJS) * 100
-    print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
-    print(pm.summary(post_normal_UIPJS))
+        print("--" * 100)
+        # LCP prior
+        LCP_model = getLCPcon(D, Ds)
+        with LCP_model:
+            post_normal_LCP = pm.sample(draws=5000, tune=5000, target_accept=0.9, cores=4, chains=4)
+        result["LCP"] = post_normal_LCP
+        print("The results of LCP")
+        diverging = post_normal_LCP['diverging']
+        print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
+        diverging_pct = diverging.nonzero()[0].size / len(post_normal_LCP) * 100
+        print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
+        print(pm.summary(post_normal_LCP))
+
+        print("--" * 100)
+        # UIPD prior
+        UIPD_model = getUIPDcon(D, Ds)
+        with UIPD_model:
+            post_normal_UIPD = pm.sample(draws=5000,tune=5000, target_accept=0.8, cores=4, chains=4)
+        result["UIPD"] = post_normal_UIPD
+        print("The results of UIPD")
+        diverging = post_normal_UIPD['diverging']
+        print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
+        diverging_pct = diverging.nonzero()[0].size / len(post_normal_UIPD) * 100
+        print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
+        print(pm.summary(post_normal_UIPD))
+
+        print("--" * 100)
+        # UIPJS prior
+        UIPJS_model = getUIPJScon(D, Ds)
+        with UIPJS_model:
+            post_normal_UIPJS = pm.sample(draws=5000, tune=5000, target_accept=0.8, cores=4, chains=4)
+        result["UIPJS"] = post_normal_UIPJS
+        print("The results of UIPJS")
+        diverging = post_normal_UIPJS['diverging']
+        print('Number of Divergent Chains: {}'.format(diverging.nonzero()[0].size))
+        diverging_pct = diverging.nonzero()[0].size / len(post_normal_UIPJS) * 100
+        print('Percentage of Divergent Chains: {:.1f}'.format(diverging_pct))
+        print(pm.summary(post_normal_UIPJS))
+
+        results.append(result)
 
 
-
-    print(f"The iteration {jj+1}/{Num}."
-        )
-    results.append(result)
-
-
-with open(f"MCMCNorm_{Num}_p0{int(100*theta0)}_n{int(n)}.pkl", "wb") as f:
-    pickle.dump(results, f)
+        with open(fname, "wb") as f:
+            pickle.dump(results, f)
